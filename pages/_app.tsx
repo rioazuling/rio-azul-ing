4ac1d3e7631225c4a11eff72fa0e7c0a6532cee2
guillermouzner <guillermouz16@gmail.com>
2022-12-15 21:44:55 -0300

    starter-template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {Inter} from "@next/font/google";
+
+const interVariable = Inter();
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <main className={interVariable.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
