dcabfd2e478607f604acd7818fa67d12e2702d3c
Mauricio Unzaga <mauriciounzaga@MacBook-Pro-de-Mauri.local>
2023-02-13 15:46:29 -0300

    rio-azul

--- pages/obras.tsx
+++ pages/obras.tsx
@@ -1,6 +1,7 @@
 import CardObras from "../components/CardObras";
 import Container from "../components/Container";
 import obra1 from "../public/obra1.png";
+import probando123 from "../public/probando123.jpg";
 
 export default function Obras() {
   return (
@@ -9,7 +10,7 @@ export default function Obras() {
         <div className="flex flex-col justify-center max-w-3xl content-center mx-auto">
           <div className="flex flex-col mt-5 items-center">
             <h1 className="uppercase text-xl font-bold">Nuestras Obras</h1>
-            <div className="h-2 w-16 sm:w-20 rounded-md bg-blue-500 mb-4" />
+            <div className="h-2 w-16 sm:w-20 rounded-md s" />
           </div>
           {/*  */}
           <div className="grid w-full grid-cols-1 gap-4 my-2 mt-4 sm:grid-cols-2">
@@ -33,6 +34,13 @@ export default function Obras() {
               }
               imageSRC={obra1}
               title={"Sistema ampliable"}
+            />
+            <CardObras
+              description={
+                "hola buenos dias  siempre las futuras ampliaciones requeridas por el cliente, logrando así unificar las distintas etapas durante la evolución de todo el proyecto."
+              }
+              imageSRC={probando123}
+              title={"hola buen dia  ampliable"}
             />
           </div>
         </div>
